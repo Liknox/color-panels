805346a59950362215873b8372567c1f08b4906d
Nazar Koval <n.kovaldev@gmail.com>
2022-12-05 08:26:08 +0200

    added: js color functions

---
+++ app.js
@@ -0,0 +1,16 @@
+const cols = document.querySelectorAll(".col")
+
+function generateRandomColor() {
+	const hexCodes = "0123456789ABCDEF"
+
+	let color = ""
+	for (let i = 0; i < 6; i++) {
+		color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+	}
+	return "#" + color
+}
+
+function setRandomColors() {
+	cols.forEach(e => (e.style.background = generateRandomColor()))
+}
+setRandomColors()
